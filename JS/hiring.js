050cda206071913c108035b43c01d0a4b70437b8
dvjnpojascodes <deviejanepojas@gmail.com>
2023-05-12 10:29:32 +0800

    modify navbar scroll effect

--- JS/hiring.js
+++ JS/hiring.js
@@ -1,4 +1,6 @@
 const navbar = document.querySelector('.navigation');
+const dropdown = document.querySelector('.nav-dropdown');
+const dropdownLinks = document.querySelector('.nav-dropdown a');
 const image = document.querySelector('.stud-image');
 const links = document.querySelectorAll('.nav-list a');
 
@@ -8,6 +10,9 @@ window.addEventListener('scroll', function() {
   if (!isScrolled && window.scrollY > 0) {
     navbar.style.backgroundColor = "#1D9FCD";
     navbar.style.color = "white";
+    dropdown.style.backgroundColor = "#1D9FCD";
+    dropdownLinks.style.color = "white";
+    
 
     links.forEach(function(link) {
         link.style.color = "white";
